6187f96ed13affe8e676624ab8bc7d56688bd05a
saiganesh-sristla <saiganesh3108@gmail.com>
2023-11-10 23:55:53 +0530

    singly linked list insertion

---
+++ singlyLLInsertion.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int data){
+      this->data = data;
+      this->next = NULL;
+    }
+};
+
+void print(Node* head){
+  Node* temp = head;
+
+  while(temp != NULL){
+    cout << temp -> data << " ";
+    temp = temp -> next;
+  }
+  cout << endl;
+}
+
+void insertAtHead(Node* &head, int data){
+
+  Node* temp = new Node(data);
+  temp -> next = head;
+  head = temp;
+
+}
+
+void insertAtTail(Node* &tail, int data){
+
+  Node* temp = new Node(data);
+  tail -> next = temp;
+  tail = temp;
+
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int data){
+  if(position == 1){
+    insertAtHead(head, data);
+    return;
+  }
+
+  Node* temp = head;
+  int count = 1;
+  while(count < position-1){
+    temp = temp -> next;
+    count++;
+  }
+
+  if(temp -> next == NULL){
+    insertAtTail(tail, data);
+    return;
+  }
+
+  Node* NodeToInsert = new Node(data);
+  NodeToInsert -> next = temp -> next;
+  temp -> next = NodeToInsert;
+}
+
+int main() {
+  Node* Node1 = new Node(10);
+  Node* head = Node1;
+  Node* tail = Node1;
+
+  print(head);
+  insertAtHead(head, 20);
+  print(head);
+
+  insertAtTail(tail, 30);
+  print(head);
+
+  insertAtPosition(head, tail, 3, 22);
+  print(head);
+
+  insertAtPosition(head, tail, 1, 33);
+  print(head);
+
+  insertAtPosition(head, tail, 6, 44);
+  print(head);
+
+  return 0;
+}
